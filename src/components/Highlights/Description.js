2c873a7c069a9c6724b073860642af51d70f503c
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2021-01-18 20:47:37 -0500

    added description component

---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description=()=>{
+    return(
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                    {/* PUT HIGHLIGHT INFORMATION FOR THE SITE HERE */}
+                    Space for description
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
