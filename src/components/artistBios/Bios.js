00b074fcedcdd69ab9d0df458831e34fce5b3289
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2021-01-20 22:32:12 -0500

    adding artist bio folder

---
+++ src/components/artistBios/Bios.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+
+
+const Bios=()=>{
+    return(
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                    {/* PUT HIGHLIGHT INFORMATION FOR THE SITE HERE */}
+                    Space for description
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
