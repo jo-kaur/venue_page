7e22db9ebf7e78c1b6250e2817084fc13dc7e08f
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2021-01-22 01:44:21 -0500

    added dates to calendar

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -7,6 +7,7 @@ export default class Calendar extends React.Component {
   render() {
     return (
         <div className="calendar">
+        {/* <center><button onClick={this.gotoDate}>Upcoming Events</button></center> */}
       <FullCalendar
         plugins={[ timeGridPlugin ]}
         initialView="timeGridWeek"
@@ -15,6 +16,13 @@ export default class Calendar extends React.Component {
         //     boxShadow: 'none',
         //     padding: '10px 0px'
         // }}
+        events={[
+          { title: 'Shawn Mendes', start: '2021-03-31 19:30', end:'2021-03-31 22:00'},
+          { title: 'BTS', start: '2021-04-01 19:30', end:'2021-04-01 22:00' },
+          { title: 'Post Malone', start: '2021-04-02 19:30', end:'2021-04-02 22:00' },
+          { title: 'Doja Cat', start: '2021-04-03 19:30', end:'2021-04-03 22:00' },
+          { title: 'Megan Thee Stallion', start: '2021-04-04 19:30', end:'2021-04-04 22:00' }
+         ]}
       />
       </div>
     )
