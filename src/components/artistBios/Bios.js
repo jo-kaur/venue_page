a0c43aca6a7aaa0202619837c1385fb7385ec9a0
Joginder Kaur <joginder.kaur@jjay.cuny.edu>
2021-01-22 00:10:18 -0500

    edited artist bio section and style

--- src/components/artistBios/Bios.js
+++ src/components/artistBios/Bios.js
@@ -10,7 +10,6 @@ import megan from '../../resources/images/artists/megan.jpg';
 
 function Bios(){
     return(
-            <div className="center_wrapper">
                 <div className="artist_wrapper">
                 <h3>Featured Artists</h3>
                 <Slide up>
@@ -20,11 +19,12 @@ function Bios(){
                                 background: `url(${shawn})`,
                                 height:`350px`,
                                 width:`50%`,
-                                position:'relative'
+                                position:'static'
                             }}
                         ></div>
-                        <div className="artist_desc">
-                            <h2>Shawn Mendes</h2>
+                            
+                        <div className="artist_desc">  
+                            <h2><a href="https://www.youtube.com/channel/UCAvCL8hyXjSUHKEGuUPr1BA">Shawn Mendes</a></h2>
                             <p>
                                 A Canadian pop artist who is known for his platinum-selling hits and Billboard-topping works. This singer and songwriter brought us hit 
                                 songs such as “Stitches”, “Believe”, “Senorita”, “If I Can’t Have You”, “Treat You Better”, “There’s Nothing Holdin Me Back”, “In My Blood”,
@@ -43,7 +43,7 @@ function Bios(){
                             }}
                         ></div>
                         <div className="artist_desc">
-                            <h2>BTS</h2>
+                            <h2><a href="https://www.youtube.com/channel/UCLkAepWjdylmXSltofFvsYQ">BTS</a></h2>
                             <p>
                                 This Korean boy band took the world by storm with countless awards and record-topping album sales.
                                 These South-Korean artists brought us hit songs such as “Dynamite”, “Boy With Luv”, “IDOL”, “FAKE LOVE”,
@@ -62,7 +62,7 @@ function Bios(){
                             }}
                         ></div>
                         <div className="artist_desc">
-                            <h2>Khalid</h2>
+                            <h2><a href="https://www.youtube.com/channel/UCkntT5Je5DDopF70YUsnuEQ">Khalid</a></h2>
                             <p>
                                 This American singer and songwriter rose to fame in 2016 with his hit song  “Location”. His music is primarily R&B, hip hop, 
                                 pop, and pop-soul. Khalid has collaborated with other artists as well such as Billie Eilish and Ed Sheeran. Some of his hit 
@@ -81,7 +81,7 @@ function Bios(){
                             }}
                         ></div>
                         <div className="artist_desc">
-                            <h2>Post Malone</h2>
+                            <h2><a href="https://www.youtube.com/channel/UCeLHszkByNZtPKcaVXOCOQQ">Post Malone</a></h2>
                             <p>
                                 Post Malone is an American rapper, singer, songwriter, and record producer. He is known for his introspective songwriting and laconic 
                                 vocal style. This versatile singer has brought us hits like “Sunflower”, “Better Now”, “Wow”, “Rockstar”, “Congratulations”, “Psycho”, 
@@ -100,7 +100,7 @@ function Bios(){
                             }}
                         ></div>
                         <div className="artist_desc">
-                            <h2>Doja Cat</h2>
+                            <h2><a href="https://www.youtube.com/channel/UCzpl23pGTHVYqvKsgY0A-_w">Doja Cat</a></h2>
                             <p>
                                 Doja Cat is a rapper, singer, songwriter, and record producer who became a viral Internet sensation after releasing
                                 her song “Mooo!” even though she had been singing before that. Some of her hit are “Say So”, “Boss B*tch”, “Like That”,
@@ -119,7 +119,7 @@ function Bios(){
                             }}
                         ></div>
                         <div className="artist_desc">
-                            <h2>Megan Thee Stallion</h2>
+                            <h2><a href="https://www.youtube.com/channel/UCKrdjiuS66yXOdEZ_cOD_TA">Megan Thee Stallion</a></h2>
                             <p>
                                 Megan Thee Stallion is a rapper as well who is famous for her infectious confidence and her jaw-dropping lyrics. She rose to 
                                 fame through her social media when videos of her freestyling became popular. Some of her hits are “Body”, “Cry Baby”, “Savage”,
@@ -129,7 +129,7 @@ function Bios(){
                     </div>
                     </Slide>
                 </div>  
-            </div>
+    
     );
 };
 
