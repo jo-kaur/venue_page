e475a22fc8633f488ed668a003d3f15e3c0d9f27
Britney <Wu.Britney@gmail.com>
2023-01-02 13:12:58 -0500

    Converted Calendar.jsx to functional component

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
-// import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 
-export default class Calendar extends React.Component {
-  render() {
-    return (
-        <div className='calendar'>
-        {/* <center><button onClick={this.gotoDate}>Upcoming Events</button></center> */}
+function Calendar() {
+  return(
+    <div className='calendar'>
       <FullCalendar
         plugins={[ timeGridPlugin ]}
         initialView='timeGridWeek'
-        // style={{
-        //     backgroundColor: this.state.headerShow ? '#240090' : 'transparent',
-        //     boxShadow: 'none',
-        //     padding: '10px 0px'
-        // }}
         events={[
           { title: 'Shawn Mendes', start: '2021-03-31 19:30', end:'2021-03-31 22:00'},
           { title: 'BTS', start: '2021-04-01 19:30', end:'2021-04-01 22:00' },
           { title: 'Post Malone', start: '2021-04-02 19:30', end:'2021-04-02 22:00' },
           { title: 'Doja Cat', start: '2021-04-03 19:30', end:'2021-04-03 22:00' },
           { title: 'Megan Thee Stallion', start: '2021-04-04 19:30', end:'2021-04-04 22:00' }
-         ]}
+        ]}
       />
-      </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default Calendar
