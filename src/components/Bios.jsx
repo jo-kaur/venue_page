1ada2c3bdaad48e4c17b6e4f38b534642a2b4bc5
Britney <Wu.Britney@gmail.com>
2023-01-03 10:56:04 -0500

    Cleaned up Bios.jsx

--- src/components/Bios.jsx
+++ src/components/Bios.jsx
@@ -9,11 +9,11 @@ import Megan from '../../resources/images/artists/megan.jpg'
 
 function Bios() {
     return(
-        <div className="artist_wrapper">
+        <div className='artist_wrapper'>
             <h3>Featured Artists</h3>
                 <Slide up>
-                    <div className="artist">
-                        <div className="artist_image" 
+                    <div className='artist'>
+                        <div className='artist_image'
                             style={{
                                 background: `url(${Shawn})`,
                                 height:`350px`,
@@ -21,7 +21,7 @@ function Bios() {
                                 position:'static'
                             }}
                         ></div>
-                        <div className="artist_desc">  
+                        <div className='artist_desc'>  
                             <h2><a href="https://www.youtube.com/channel/UCAvCL8hyXjSUHKEGuUPr1BA">Shawn Mendes</a></h2>
                             <p>
                                 A Canadian pop artist who is known for his platinum-selling hits and Billboard-topping works. This singer and songwriter brought us hit 
@@ -30,8 +30,8 @@ function Bios() {
                             </p>
                         </div>
                     </div>   
-                    <div className="artist">
-                        <div className="artist_image"
+                    <div className='artist'>
+                        <div className='artist_image'
                             style={{
                                 background: `url(${Bts})`,
                                 height:`350px`,
@@ -39,7 +39,7 @@ function Bios() {
                                 position:'relative'
                             }}
                         ></div>
-                        <div className="artist_desc">
+                        <div className='artist_desc'>
                             <h2><a href="https://www.youtube.com/channel/UCLkAepWjdylmXSltofFvsYQ">BTS</a></h2>
                             <p>
                                 This Korean boy band took the world by storm with countless awards and record-topping album sales.
@@ -48,8 +48,8 @@ function Bios() {
                             </p>
                         </div>
                     </div>
-                    <div className="artist">
-                        <div className="artist_image"
+                    <div className='artist'>
+                        <div className='artist_image'
                             style={{
                                 background: `url(${Khalid})`,
                                 height:`350px`,
@@ -57,8 +57,8 @@ function Bios() {
                                 position:'relative'
                             }}
                         ></div>
-                        <div className="artist_desc">
-                            <h2><a href="https://www.youtube.com/channel/UCkntT5Je5DDopF70YUsnuEQ">Khalid</a></h2>
+                        <div className='artist_desc'>
+                            <h2><a href='https://www.youtube.com/channel/UCkntT5Je5DDopF70YUsnuEQ'>Khalid</a></h2>
                             <p>
                                 This American singer and songwriter rose to fame in 2016 with his hit song  “Location”. His music is primarily R&B, hip hop, 
                                 pop, and pop-soul. Khalid has collaborated with other artists as well such as Billie Eilish and Ed Sheeran. Some of his hit 
@@ -66,8 +66,8 @@ function Bios() {
                             </p>
                         </div>
                     </div>
-                    <div className="artist">
-                        <div className="artist_image"
+                    <div className='artist'>
+                        <div className='artist_image'
                             style={{
                                 background: `url(${PostMalone})`,
                                 height:`350px`,
@@ -75,8 +75,8 @@ function Bios() {
                                 position:'relative'
                             }}
                         ></div>
-                        <div className="artist_desc">
-                            <h2><a href="https://www.youtube.com/channel/UCeLHszkByNZtPKcaVXOCOQQ">Post Malone</a></h2>
+                        <div className='artist_desc'>
+                            <h2><a href='https://www.youtube.com/channel/UCeLHszkByNZtPKcaVXOCOQQ'>Post Malone</a></h2>
                             <p>
                                 Post Malone is an American rapper, singer, songwriter, and record producer. He is known for his introspective songwriting and laconic 
                                 vocal style. This versatile singer has brought us hits like “Sunflower”, “Better Now”, “Wow”, “Rockstar”, “Congratulations”, “Psycho”, 
@@ -84,8 +84,8 @@ function Bios() {
                             </p>
                         </div>
                     </div>
-                    <div className="artist">
-                        <div className="artist_image"
+                    <div className='artist'>
+                        <div className='artist_image'
                             style={{
                                 background: `url(${Doja})`,
                                 height:`350px`,
@@ -93,8 +93,8 @@ function Bios() {
                                 position:'relative'
                             }}
                         ></div>
-                        <div className="artist_desc">
-                            <h2><a href="https://www.youtube.com/channel/UCzpl23pGTHVYqvKsgY0A-_w">Doja Cat</a></h2>
+                        <div className='artist_desc'>
+                            <h2><a href='https://www.youtube.com/channel/UCzpl23pGTHVYqvKsgY0A-_w'>Doja Cat</a></h2>
                             <p>
                                 Doja Cat is a rapper, singer, songwriter, and record producer who became a viral Internet sensation after releasing
                                 her song “Mooo!” even though she had been singing before that. Some of her hit are “Say So”, “Boss B*tch”, “Like That”,
@@ -102,8 +102,8 @@ function Bios() {
                             </p>
                         </div>
                     </div>
-                    <div className="artist">
-                        <div className="artist_image"
+                    <div className='artist'>
+                        <div className='artist_image'
                             style={{
                                 background: `url(${Megan})`,
                                 height:`350px`,
@@ -111,8 +111,8 @@ function Bios() {
                                 position:'relative'
                             }}
                         ></div>
-                        <div className="artist_desc">
-                            <h2><a href="https://www.youtube.com/channel/UCKrdjiuS66yXOdEZ_cOD_TA">Megan Thee Stallion</a></h2>
+                        <div className='artist_desc'>
+                            <h2><a href='https://www.youtube.com/channel/UCKrdjiuS66yXOdEZ_cOD_TA'>Megan Thee Stallion</a></h2>
                             <p>
                                 Megan Thee Stallion is a rapper as well who is famous for her infectious confidence and her jaw-dropping lyrics. She rose to 
                                 fame through her social media when videos of her freestyling became popular. Some of her hits are “Body”, “Cry Baby”, “Savage”,
